fe9a6dc428ac4a7bf7c4ec967f1e4ce9247a21bb
Ye Thiha <mightycryptowall@gmail.com>
2022-09-27 12:40:22 +0630

    prisma database added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,17 @@
 import { AuthService } from './auth.service';
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signup() {
+    return 'I am signup';
+  }
+
+  @Post('signin')
+  signin() {
+    return 'I am signin';
+  }
 }
